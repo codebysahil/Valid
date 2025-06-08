dffa2bffacf1f655bc83bd371c6f1e3201d1a437
Sahil Satija <96464952+codebysahil@users.noreply.github.com>
2025-06-08 23:14:39 +0200

    Update DriverFactory.java
    Correct DriverFactory

--- src/main/java/validaide/qa/website/factory/DriverFactory.java
+++ src/main/java/validaide/qa/website/factory/DriverFactory.java
@@ -1,77 +1,64 @@
-package validaide.qa.website.factory;
-import java.util.Properties;
+package factory;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
 	
 	public WebDriver driver;
-	public Properties prop ;
-	/**
-	 * this method is used to launch the diver on the bases of provided browser name
-	 * @param browserName
-	 * @return
-	 * @throws InterruptedException 
-	 */
+	public Properties prop;
+	public OptionsManager om;
 	
-	public WebDriver initDriver(String browserName) throws InterruptedException {
+	
+	public WebDriver launchBrowser(Properties prop) {
 		
-		if(browserName.equalsIgnoreCase("chrome")){
-			driver = new ChromeDriver();
+		String bname = prop.getProperty("browser");
+		om= new OptionsManager(prop);
+		
+		if(bname.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver(om.getChromeOptions());
+		}
+		else if (bname.equalsIgnoreCase("firefox")) {
+			driver= new FirefoxDriver(om.getFirefoxOptions());
 		}
 		
-		else if (browserName.equalsIgnoreCase("safari"))
-		{
-			driver=new SafariDriver();
-		}
-		else {
-			System.out.println("Please pass a correct browser name  "+ " "+ browserName);
-			
-		}
 		
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // why this has been added
-		
+		driver.get("https://www.validaide.com/");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
 		driver.manage().deleteAllCookies();
-		
 		driver.manage().window().maximize();
 		
-		driver.get(prop.getProperty("url"));
-		Thread.sleep(1000);
 		return driver;
 		
+		
 	}
-	/**
-	 * 
-	 * @return this returns properties reference with all configuration properties
-	 */
 	
-	public Properties initprop() {
-		
-		prop = new Properties(); // to load the properties 
-		
+	
+	public Properties initProperties() {
+		prop = new Properties();
 		try {
-			FileInputStream fp = new FileInputStream("./src/test/resources/config/config.properties");
-			prop.load(fp);// to establish a connection 
+			FileInputStream fp = new FileInputStream("./src/test/resource/config/config.properties");
+			try {
+				prop.load(fp);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		return prop;
+		
+		
 	}
-	
-	
-	
-	
-	
 
 }
